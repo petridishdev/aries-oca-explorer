5894520b137f6fe9810176c498d03b6b23c6d242
Akiff Manji <amanji@petridish.dev>
2023-03-29 09:01:49 -0700

    feat: form to fetch bundle
    Signed-off-by: Akiff Manji <amanji@petridish.dev>

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
+import { useCallback, useEffect, useMemo, useState } from "react";
 import CredentialCard from "./components/CredentialCard";
+import Form from "./components/Form";
+import OverlayBundle from "./services/OverlayBundle";
 
 function App() {
+  const [overlay, setOverlay] = useState<OverlayBundle | undefined>(undefined);
+
+  const handleOverlay = useCallback((overlay: OverlayBundle) => {
+    setOverlay(overlay);
+  }, []);
+
   return (
     <div className="App">
-      <CredentialCard />
+      <Form onOverlay={handleOverlay} />
+      {overlay && <CredentialCard overlay={overlay} />}
     </div>
   );
 }
